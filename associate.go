d79af88873692d69632607b4193658ccfea7f5d9
Alec Iverson <adiverso93@gmail.com>
2023-02-15 13:51:38 -0700

    add KeyValue helper for Associate

--- associate.go
+++ associate.go
@@ -21,3 +21,38 @@ func (a Associater[In, K, V]) ByTo(to map[K]V, f func(In) (K, V)) {
 func Associate[K comparable, V any, In any](in Stream[In]) Associater[In, K, V] {
 	return Associater[In, K, V]{src: in.src}
 }
+
+// KeyValueAssociater is a helper to reduce noise when using [Associate] with [KeyValue]s.
+type KeyValueAssociater[K comparable, V any] struct {
+	src streamer[KeyValue[K, V]]
+}
+
+// By creates a map[K]V from the Stream, with keys and values defined by f.
+// If f is nil, the identity function is used (K and V without any changes).
+func (a KeyValueAssociater[K, V]) By(f func(K, V) (K, V)) map[K]V {
+	if f == nil {
+		f = func(k K, v V) (K, V) { return k, v }
+	}
+
+	m := make(map[K]V, a.src.capacityHint())
+	a.src.forEach(func(kv KeyValue[K, V]) bool {
+		k, v := f(kv.Key, kv.Val)
+		m[k] = v
+		return true
+	})
+	return m
+}
+
+// ByTo fills to from the Stream, with keys and values defined by f.
+// If f is nil, the identity function is used (K and V without any changes).
+func (a KeyValueAssociater[K, V]) ByTo(to map[K]V, f func(K, V) (K, V)) {
+	a.src.forEach(func(kv KeyValue[K, V]) bool {
+		k, v := f(kv.Key, kv.Val)
+		to[k] = v
+		return true
+	})
+}
+
+func AssociateFromKeyValue[K comparable, V any](in Stream[KeyValue[K, V]]) KeyValueAssociater[K, V] {
+	return KeyValueAssociater[K, V]{src: in.src}
+}
--- map.go
+++ map.go
@@ -8,8 +8,28 @@ type KeyValue[K comparable, V any] struct {
 // KeyValueLess provides a generic [LessFunc] for [KeyValue]s with an [Ordered] key.
 func KeyValueLess[K Ordered, V any](lhs, rhs KeyValue[K, V]) bool { return lhs.Key < rhs.Key }
 
-// KeyValueAssociate provides a default function for [KeyValue]s for [Associate].
-func KeyValueAssociate[K comparable, V any](kv KeyValue[K, V]) (K, V) { return kv.Key, kv.Val }
+// AssociateKeyValue provides a default function for [KeyValue]s for [Associate].
+func AssociateKeyValue[K comparable, V any](kv KeyValue[K, V]) (K, V) { return kv.Key, kv.Val }
+
+// ValueTransform is a helper wrapper to simplify transforming only the value of a [KeyValue].
+func ValueTransform[K comparable, V any](f func(V) V) func(KeyValue[K, V]) KeyValue[K, V] {
+	return func(kv KeyValue[K, V]) KeyValue[K, V] {
+		return KeyValue[K, V]{
+			Key: kv.Key,
+			Val: f(kv.Val),
+		}
+	}
+}
+
+// ValueMapper is a helper wrapper to simplify mapping only the value of a [KeyValue].
+func ValueMapper[OutV any, K comparable, InV any](f func(InV) OutV) func(KeyValue[K, InV]) KeyValue[K, OutV] {
+	return func(kv KeyValue[K, InV]) KeyValue[K, OutV] {
+		return KeyValue[K, OutV]{
+			Key: kv.Key,
+			Val: f(kv.Val),
+		}
+	}
+}
 
 func FromMap[K comparable, V any, M ~map[K]V](src M) Stream[KeyValue[K, V]] {
 	return Stream[KeyValue[K, V]]{
